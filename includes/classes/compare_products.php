0076fe84801c9a0335295e5bc5b07d7458049ded
jack.yin <jack.yin@tomatocart.com>
2012-06-24 06:50:57 +0000

    Jack commit - Fix the issues in compare products - for the variants products, the product image is wrong and the add to cart button is not usable. Moreover, improve the layout

--- includes/classes/compare_products.php
+++ includes/classes/compare_products.php
@@ -57,17 +57,23 @@
       
       $content = '';
       
-      $products_images = '';
-      $products_titles = '';
-      $products_price = '';
-      $products_weight = '';
-      $products_sku = '';
-      $products_manufacturers = '';
-      $products_desciptions = '';
-	    $products_attributes = '';
+      $products_images = array();
+      $products_titles = array();
+      $products_price = array();
+      $products_weight = array();
+      $products_sku = array();
+      $products_manufacturers = array();
+      $products_desciptions = array();
+	    $products_attributes = array();
+	    $products_variants = array();
+	    
+	    $cols = array('<col width="20%">');
+	    $col_width = round(80 / count($this->getProducts()));
 	    
       if ($this->hasContents()) {
         foreach ($this->getProducts() as $products_id) {
+          $cols[] = '<col width="' . $col_width . '%">';
+          
           $osC_Product = new osC_Product($products_id);
           
           $image = $osC_Product->getImages();
@@ -78,12 +84,11 @@
           
          //if the product have any variants, it means that the $products_id should be a product string such as 1#1:1;2:2
           if ($osC_Product->hasVariants()) {
-            $str_variants = '';
-             
-            $products_variants = $osC_Product->getVariants();
+            $product_variants = $osC_Product->getVariants();
             $variants = array();
+            
             if (preg_match('/^[0-9]+(#?([0-9]+:?[0-9]+)+(;?([0-9]+:?[0-9]+)+)*)+$/', $products_id)) {
-              $products_variant = $products_variants[$products_id];
+              $products_variant = $product_variants[$products_id];
               
               $variants = osc_parse_variants_from_id_string($products_id);
             }else {
@@ -92,69 +97,210 @@
             
             //if the product have any variants, get the group_name:value_name string
             if (isset($products_variant) && isset($products_variant['groups_name']) && is_array($products_variant['groups_name']) && !empty($products_variant['groups_name'])) {
-              $str_variants .= ' -- ';
+              $products_variants[$products_id]['variants'] = array();
               
               foreach($products_variant['groups_name'] as $groups_name => $value_name) {
-                $str_variants .= '<strong>' . $groups_name . ': ' . $value_name . '</strong>;';
-              }
-              
-              //clean the last ';'
-              if (($pos = strrpos($str_variants, ';')) !== false) {
-                $str_variants = substr($str_variants, 0, -1);
+                $products_variants[$products_id]['variants'][] = array('name' => $groups_name, 'value' => $value_name);
               }
             }
             
-            $product_title .= $str_variants;
             $product_price = $osC_Currencies->displayPrice($osC_Product->getPrice($variants), $osC_Product->getTaxClassID());
             $product_weight = $products_variant['weight'];
             $product_sku = $products_variant['sku'];
+            $image = $products_variant['image'];
           }
           
-          $products_images .= '<td width="120" valign="top" align="center">' . osc_link_object(osc_href_link(FILENAME_PRODUCTS, $products_id), $osC_Image->show($image[0]['image'], $osC_Product->getTitle())) . '<br /><br />' . osc_link_object(osc_href_link(FILENAME_PRODUCTS, $products_id . '&action=cart_add'), osc_draw_image_button('button_in_cart.gif', $osC_Language->get('button_add_to_cart'))) . '</td>';
-          $products_titles .= '<td valign="top" align="center">' . $product_title . '</td>';
-          $products_price .= '<td valign="top" align="center">' . $product_price . '</td>';
-          $products_weight .= '<td valign="top" align="center">' . $osC_Weight->display($product_weight, $osC_Product->getWeightClass()) . '</td>';
-          $products_sku .= '<td valign="top" align="center">' . $product_sku . '</td>';
-          $products_manufacturers .= '<td valign="top" align="center">' . $osC_Product->getManufacturer() . '</td>';
-          $products_desciptions .= '<td valign="top" align="center">' . $osC_Product->getDescription() . '</td>';
+          $image = (is_array($image) ? $image[0]['image'] : $image);
+          
+          $products_titles[] = $product_title;
+          
+          if (!osc_empty($product_price)) {
+            $products_price[] = $product_price;
+          }
+          
+          if (!osc_empty($product_weight)) {
+            $products_weight[] = $osC_Weight->display($product_weight, $osC_Product->getWeightClass());
+          }
+          
+          if (!osc_empty($product_sku)) {
+            $products_sku[] = $product_sku;
+          }
+          
+          if (!osc_empty($osC_Product->getManufacturer()))  {
+            $products_manufacturers[] = $osC_Product->getManufacturer();
+          }
+          
+          if (!osc_empty($osC_Product->getDescription()))  {
+            $products_desciptions[] = $osC_Product->getDescription();
+          }
           
           if ( $osC_Product->hasAttributes() ) {
             foreach ( $osC_Product->getAttributes() as $attribute) {
-              $products_attributes[$attribute['name']][$products_id] = $attribute['value'];
+              $products_attributes[$products_id]['attributes'][] = array('name' => $attribute['name'], 'value' => $attribute['value']);
             }
           }
+          
+          $products_id = str_replace('#', '_', $products_id);
+          
+          $products_images[] = '<div class="image">' . osc_link_object(osc_href_link(FILENAME_PRODUCTS, $products_id), $osC_Image->show($image, $osC_Product->getTitle())) . '</div>' .
+                               '<div class="button">' . osc_link_object(osc_href_link(FILENAME_PRODUCTS, $products_id . '&action=cart_add'), osc_draw_image_button('button_in_cart.gif', $osC_Language->get('button_add_to_cart'))) . '</div>';
         }
         
         $content .= '<table id="compareProducts" cellspacing="0" cellpadding="2" border="0">';
-        $content .= '<tr class="odd"><td width="120">&nbsp;</td>' . $products_images . '</tr>';
-        $content .= '<tr class="even"><td valign="top" align="left" class="label">' . $osC_Language->get('field_products_name') . '</td>' . $products_titles . '</tr>';
-        $content .= '<tr class="odd"><td valign="top" align="left" class="label">' . $osC_Language->get('field_products_price') . '</td>' . $products_price . '</tr>';
-        $content .= '<tr class="even"><td valign="top" align="left" class="label">' . $osC_Language->get('field_products_weight') . '</td>' . $products_weight . '</tr>';
-        $content .= '<tr class="odd"><td valign="top" align="left" class="label">' . $osC_Language->get('field_products_sku') . '</td>' . $products_sku . '</tr>';
-        $content .= '<tr class="even"><td valign="top" align="left" class="label">' . $osC_Language->get('field_products_manufacturer') . '</td>' . $products_manufacturers . '</tr>';
         
-        if(!empty($products_attributes)) {
-	        $rows = 0;
-	        foreach($products_attributes as $name => $attribute) {
-	          $content .= '<tr class="' . ((($rows/2) == floor($rows/2)) ? 'odd' : 'even') . '">';
-	          $content .= '<td valign="top" align="left" class="label">' . $name . ':</td>';
-	                
-	          foreach ($this->getProducts() as $products_id) {
-	            if (isset($attribute[$products_id])) {
-	              $content .= '<td align = "center">' . $attribute[$products_id] . '</td>';
-	            } else {
-	              $content .= '<td align = "center"> -- </td>';
-	            }
-	          }
-	
-	          $content .= '</tr>';
-	          
-	          $rows++;
-	        }
+        //add col groups
+        $content .= '<colgroup>';
+        foreach($cols as $col) {
+          $content .= $col;
         }
-
-        $content .= '<tr class="' . ((($rows/2) == floor($rows/2)) ? 'odd' : 'even') . '"><td valign="top" align="left" class="label">' . $osC_Language->get('field_products_description') . '</td>' . $products_desciptions . '</tr>';
-        $content .= '</table></div>';
+        $content .= '</colgroup>';
+        
+        //add product header
+        $content .= '<tbody>';
+        $content .= '<tr class="first">';
+        $content .= '<th>&nbsp;</th>';
+        
+        if (!osc_empty($products_images)) {
+          foreach($products_images as $k => $product_image) {
+            $content .= '<td' . ($k == (count($products_images) - 1) ? ' class="last"' : '') . '>' . $product_image . '</td>';
+          }
+        }
+        $content .= '</tr>';
+        $content .= '</tbody>';
+        
+        //add compare details
+        $content .= '<tbody>';
+        
+        $row_class='even';
+        
+        //add product name
+        if (!osc_empty($products_titles)) {
+          $content .= '<tr class="' . $row_class . '">' .
+                        '<th>' . $osC_Language->get('field_products_name') . '</th>';
+          
+          foreach($products_titles as $k => $product_title) {
+            $content .= '<td' . ($k == (count($products_titles) - 1) ? ' class="last"' : '') . '>' . $product_title . '</td>';
+          }
+          
+          $content .= '</tr>';
+          
+          $row_class = ($row_class == 'even' ? 'odd' : 'even');
+        }
+        
+        //add product price
+        if (!osc_empty($products_price)) {
+          $content .= '<tr class="' . $row_class . '">' .
+                        '<th>' . $osC_Language->get('field_products_price') . '</th>';
+          
+          foreach($products_price as $k => $product_price) {
+            $content .= '<td' . ($k == (count($products_price) - 1) ? ' class="last"' : '') . '>' . $product_price . '</td>';
+          }
+          
+          $content .= '</tr>';
+          
+          $row_class = ($row_class == 'even' ? 'odd' : 'even');
+        }
+        
+        //add product weight
+        if (!osc_empty($products_weight)) {
+          $content .= '<tr class="' . $row_class . '">' .
+                        '<th>' . $osC_Language->get('field_products_weight') . '</th>';
+          
+          foreach($products_weight as $k => $product_weight) {
+            $content .= '<td' . ($k == (count($products_weight) - 1) ? ' class="last"' : '') . '>' . $product_weight . '</td>';
+          }
+          
+          $content .= '</tr>';
+          
+          $row_class = ($row_class == 'even' ? 'odd' : 'even');
+        }
+        
+        //add product sku
+        if (!osc_empty($products_sku)) {
+          $content .= '<tr class="' . $row_class . '">' .
+                        '<th>' . $osC_Language->get('field_products_sku') . '</th>';
+          
+          foreach($products_sku as $k => $product_sku) {
+            $content .= '<td' . ($k == (count($products_sku) - 1) ? ' class="last"' : '') . '>' . $product_sku . '</td>';
+          }
+          
+          $content .= '</tr>';
+          
+          $row_class = ($row_class == 'even' ? 'odd' : 'even');
+        }
+        
+        //add product manufacturers
+        if (!osc_empty($products_manufacturers)) {
+          $content .= '<tr class="' . $row_class . '">' .
+                        '<th>' . $osC_Language->get('field_products_manufacturer') . '</th>';
+          
+          foreach($products_manufacturers as $k => $product_manufacturer) {
+            $content .= '<td' . ($k == (count($products_manufacturers) - 1) ? ' class="last"' : '') . '>' . $product_manufacturer . '</td>';
+          }
+          
+          $content .= '</tr>';
+          
+          $row_class = ($row_class == 'even' ? 'odd' : 'even');
+        }
+        
+       //add product variants
+        if (!osc_empty($products_variants)) {
+          $content .= '<tr class="' . $row_class . '">' .
+                      '<th>' . $osC_Language->get('field_products_variants') . '</th>';
+          
+          foreach($this->getProducts() as $k => $products_id) {
+            if (isset($products_variants[$products_id]['variants']) && !osc_empty($products_variants[$products_id]['variants'])) {
+              
+              $content .= '<td' . ($k == (count($this->getProducts()) - 1) ? ' class="last"' : '') . '>';
+              foreach($products_variants[$products_id]['variants'] as $variant) {
+                $content .= '<span class="variant">' . $variant['name'] . ': ' . $variant['value'] . '</span>';
+              }
+              $content .= '</td>';
+            }
+          }
+          
+          $content .= '</tr>';
+          
+          $row_class = ($row_class == 'even' ? 'odd' : 'even');
+        }
+        
+        //add product attributes
+        if (!osc_empty($products_attributes)) {
+          $content .= '<tr class="' . $row_class . '">' .
+                      '<th>' . $osC_Language->get('field_products_attributes') . '</th>';
+          
+          foreach($this->getProducts() as $k => $products_id) {
+            if (isset($products_attributes[$products_id]['attributes']) && !osc_empty($products_attributes[$products_id]['attributes'])) {
+              
+              $content .= '<td' . ($k == (count($this->getProducts()) - 1) ? ' class="last"' : '') . '>';
+              foreach($products_attributes[$products_id]['attributes'] as $attribute) {
+                $content .= '<span class="attribute">' . $attribute['name'] . ': ' . $attribute['value'] . '</span>';
+              }
+              $content .= '</td>';
+            }
+          }
+          
+          $content .= '</tr>';
+          
+          $row_class = ($row_class == 'even' ? 'odd' : 'even');
+        }
+        
+        //add product description
+        if (!osc_empty($products_desciptions)) {
+          $content .= '<tr class="' . $row_class . ' last">' .
+                        '<th>' . $osC_Language->get('field_products_description') . '</th>';
+          
+          foreach($products_desciptions as $k => $product_description) {
+            $content .= '<td' . ($k == (count($products_desciptions) - 1) ? ' class="last"' : '') . '>' . $product_description . '</td>';
+          }
+          
+          $content .= '</tr>';
+          
+          $row_class = ($row_class == 'even' ? 'odd' : 'even');
+        }
+                
+        $content .= '</tbody>';
+        $content .= '</table>';
       }
       
       return $content;
