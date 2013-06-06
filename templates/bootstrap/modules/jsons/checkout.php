6577bdb37ed27de7b5d5952b791e3b791515fe0f
lei <lei@tomatocart.com>
2013-06-06 16:12:08 +0000

    [#77] syntax (semi colons) errors

--- templates/bootstrap/modules/jsons/checkout.php
+++ templates/bootstrap/modules/jsons/checkout.php
@@ -692,7 +692,7 @@ class toC_Json_Checkout {
     }
 
     function redeemCoupon() {
-        global $toC_Json, $osC_Language, $osC_Payment, $osC_ShoppingCart, $osC_Currencies, $osC_Payment;;
+        global $toC_Json, $osC_Language, $osC_Payment, $osC_ShoppingCart, $osC_Currencies, $osC_Payment;
 
         $osC_Language->load('checkout');
 
