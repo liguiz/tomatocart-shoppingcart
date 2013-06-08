ab97dfaf450ce40205a892eb5bd4e9dd211e3f6a
lei <lei@tomatocart.com>
2013-06-08 18:44:10 +0000

    [#82] Default currency for specified language doesn't work

--- includes/modules/services/currencies.php
+++ includes/modules/services/currencies.php
@@ -18,11 +18,11 @@
       include('includes/classes/currencies.php');
       $osC_Currencies = new osC_Currencies();
 
-      if ((isset($_SESSION['currency']) == false) || isset($_GET['currency']) || ( (USE_DEFAULT_LANGUAGE_CURRENCY == '1') && ($osC_Currencies->getCode($osC_Language->getCurrencyID()) != $_SESSION['currency']) ) ) {
+      if ((isset($_SESSION['currency']) == false) || isset($_GET['currency']) || ($osC_Currencies->getCode($osC_Language->getCurrencyID()) != $_SESSION['currency']) ) {
         if (isset($_GET['currency']) && $osC_Currencies->exists($_GET['currency'])) {
           $_SESSION['currency'] = $_GET['currency'];
         } else {
-          $_SESSION['currency'] = (USE_DEFAULT_LANGUAGE_CURRENCY == '1') ? $osC_Currencies->getCode($osC_Language->getCurrencyID()) : DEFAULT_CURRENCY;
+          $_SESSION['currency'] = $osC_Currencies->getCode($osC_Language->getCurrencyID());
         }
 
         if ( isset($_SESSION['cartID']) ) {
