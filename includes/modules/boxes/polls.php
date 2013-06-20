6903f72db2236080b7a54ac9d54f717ab0f72767
lei <lei@tomatocart.com>
2013-06-20 16:33:15 +0000

    [#88] Other Bootstrap issues found!

--- includes/modules/boxes/polls.php
+++ includes/modules/boxes/polls.php
@@ -38,7 +38,7 @@
       if ($Qpoll->numberOfRows() > 0) {
         $this->_content .= '<h6>' . $Qpoll->value('polls_title') . '</h6>';
         
-        $Qanswers = $osC_Database->query('select pa.polls_id, pa.polls_answers_id, pa.votes_count, pa.sort_order, pad.answers_title from :table_polls_answers pa, :table_polls_answers_description pad where pa.polls_id = :polls_id and pa.polls_answers_id = pad.polls_answers_id and pad.languages_id = :languages_id order by pa.sort_order desc');
+        $Qanswers = $osC_Database->query('select pa.polls_id, pa.polls_answers_id, pa.votes_count, pa.sort_order, pad.answers_title from :table_polls_answers pa, :table_polls_answers_description pad where pa.polls_id = :polls_id and pa.polls_answers_id = pad.polls_answers_id and pad.languages_id = :languages_id order by pa.sort_order asc');
         $Qanswers->bindTable(':table_polls_answers', TABLE_POLLS_ANSWERS);
         $Qanswers->bindTable(':table_polls_answers_description', TABLE_POLLS_ANSWERS_DESCRIPTION);
         $Qanswers->bindInt(':polls_id', $Qpoll->valueInt('polls_id'));
--- templates/bootstrap/modules/boxes/search.php
+++ templates/bootstrap/modules/boxes/search.php
@@ -14,6 +14,8 @@
  * @since        Version 1.1.8
  * @filesource
 */
+
+global $osC_Language;
 ?>
 
 <!-- box search start //-->
@@ -21,7 +23,13 @@
 <div class="boxNew">
     <div class="boxTitle"><?php echo osc_link_object($osC_Box->getTitleLink(), $osC_Box->getTitle()); ?></div>
     
-    <div class="boxContents" style="text-align: center;"><?php echo $osC_Box->getContent(); ?></div>
+    <div class="boxContents" style="text-align: center;">
+		<form name="search" action="<?php echo osc_href_link(FILENAME_SEARCH, null, 'NONSSL', false); ?>" method="get" style="line-height: 30px">
+			<?php echo osc_draw_input_field('keywords', null, 'style="width: 78%;" maxlength="30"') . '&nbsp;' . osc_draw_hidden_session_id_field() . osc_draw_image_submit_button('button_quick_find.gif', $osC_Language->get('box_search_heading')); ?>
+			<br />
+			<?php sprintf($osC_Language->get('box_search_text'), osc_href_link(FILENAME_SEARCH)); ?>
+		</form>
+    </div>
 </div>
 
 <!-- box search end //-->
