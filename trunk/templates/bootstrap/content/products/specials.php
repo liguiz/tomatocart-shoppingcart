a04201c69a565276a274d83a757b4e47cb03b2a1
jack.yin@tomatocart.com <jack.yin@tomatocart.com@f39f1ae9-788a-612c-334f-b445401a6527>
2013-07-05 07:51:36 +0000

    [#99]Bootstrap don't support list/grid view for specials page!

--- trunk/templates/bootstrap/content/products/specials.php
+++ trunk/templates/bootstrap/content/products/specials.php
@@ -15,54 +15,11 @@
  * @filesource
 */
 
-  $Qspecials = osC_Specials::getListing();
+  $Qlisting = osC_Specials::getListing();
 ?>
 
 <h1><?php echo $osC_Template->getPageTitle(); ?></h1>
 
-<div class="moduleBox">
-	<h6><?php echo $osC_Template->getPageTitle(); ?></h6>
-	
-    <?php if ($Qspecials->numberOfRows() > 0) { ?>
-    <ul class="products-list grid clearfix">
-    
-        <?php
-            while ($Qspecials->next()) {
-                $osC_Product = new osC_Product($Qspecials->value('products_id'));
-        ?>
-        <li class="clearfix">
-            <div class="left">
-                <?php echo osc_link_object(osc_href_link(FILENAME_PRODUCTS, $Qspecials->value('products_id')), $osC_Image->show($Qspecials->value('image'), $Qspecials->value('products_name'))); ?> 
-                <h3><?php echo osc_link_object(osc_href_link(FILENAME_PRODUCTS, $Qspecials->value('products_id')), $Qspecials->value('products_name')); ?></h3>
-                <p class="description"><?php echo strip_tags($osC_Product->getDescription()); ?></p>
-            </div>
-            <div class="right">
-                <span class="price"><?php echo $osC_Product->getPriceFormated(true); ?></span>
-                <span class="buttons hidden-phone">
-                    <a class="btn btn-small btn-info" href="<?php echo osc_href_link(FILENAME_PRODUCTS, $Qspecials->value('products_id') . '&action=cart_add'); ?>">
-                    	<i class="icon-shopping-cart icon-white "></i> 
-                    	<?php echo $osC_Language->get('button_buy_now'); ?>
-                    </a>
-                    <br />
-                    <?php echo osc_link_object(osc_href_link(basename($_SERVER['SCRIPT_FILENAME']), $Qspecials->value('products_id') . '&action=wishlist_add'), $osC_Language->get('add_to_wishlist'), 'class="wishlist"'); ?>
-                    <?php
-                      if ($osC_Template->isInstalled('compare_products', 'boxes')) {
-                          echo  '<br />' . osc_link_object(osc_href_link(basename($_SERVER['SCRIPT_FILENAME']), osc_get_all_get_params() . '&cid=' . $Qspecials->value('products_id') . '&' . '&action=compare_products_add'), $osC_Language->get('add_to_compare'), 'class="compare"');
-                      }
-                    ?>
-                </span>
-            </div>
-		</li>
-      <?php 
-        }
-      ?>
-      
-    </ul>
-  
-    <div class="listingPageLinks">
-      <span style="float: right;"><?php echo $Qspecials->getBatchPageLinks('page', 'specials'); ?></span>
-    
-      <?php echo $Qspecials->getBatchTotalPages($osC_Language->get('result_set_number_of_products')); ?>
-    </div>
-	<?php } ?>
-</div>
+<?php
+  include('templates/' . $osC_Template->getCode() . '/modules/product_listing.php');
+?>
