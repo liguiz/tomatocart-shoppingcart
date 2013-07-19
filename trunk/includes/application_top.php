371ff4636edf3a9f9763fd60f0a39aeb7e366912
jack.yin@tomatocart.com <jack.yin@tomatocart.com@f39f1ae9-788a-612c-334f-b445401a6527>
2013-07-19 07:16:34 +0000

    [#107]PHP 5.4 Strict Warning Issue

--- trunk/includes/application_top.php
+++ trunk/includes/application_top.php
@@ -16,7 +16,7 @@
   
 // set the level of error reporting to E_ALL except E_NOTICE
   if (version_compare(PHP_VERSION, '5.3.0') >= 0) {
-    error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);
+    error_reporting(E_ALL & ~E_STRICT & ~E_NOTICE & ~E_DEPRECATED);
   } else {
     error_reporting(E_ALL & ~E_NOTICE);
   }
